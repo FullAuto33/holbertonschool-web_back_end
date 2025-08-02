17ce1a28d312a12e4ad9ff784c920af424691b8b
FullAuto33 <46572123+FullAuto33@users.noreply.github.com>
2025-08-02 13:43:47 +0200

    Script return list of delays

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" function task_wait_n"""
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> list[float]:
+    """return list of delays"""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]  # create tasks
+    delays = await asyncio.gather(*tasks)  # wait for all of them
+    return sorted(delays)
