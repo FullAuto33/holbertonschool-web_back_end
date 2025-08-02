ae2280ce18a2ea352f4e715993808ef14e47f0d9
root <djelloulrouba@yahoo.com>
2025-08-02 10:37:48 +0000

    Script concat two str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function concat that takes a string str1 and a string str2"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns the concatenation of str1 and str2"""
+    return str1 + str2
