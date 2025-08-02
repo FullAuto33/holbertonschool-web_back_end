88d6ffd51019862913f1512834a4996962ed6f36
FullAuto33 <46572123+FullAuto33@users.noreply.github.com>
2025-08-02 13:53:15 +0200

    update not use sorted script use boucle for

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,6 +6,10 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> list[float]:
     """return list of delays"""
-    tasks = [task_wait_random(max_delay) for _ in range(n)]  # create tasks
-    delays = await asyncio.gather(*tasks)  # wait for all of them
-    return sorted(delays)
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for completed in asyncio.as_completed(tasks):
+        delay = await completed
+        delays.append(delay)
+    return delays
+
